47e5ff20a05aa660d08ab99372ee31e6a624c99b
A-One <102692354+A-One0@users.noreply.github.com>
2025-10-03 11:13:08 +0200

    Update methoderessoure.js

--- api/methoderessoure.js
+++ api/methoderessoure.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   const { method: meth, url, headers: heads, sendBody } = req.body;
 
-  console.log(typeof(meth), typeof(utl), typeof(heads);
+  console.log(typeof meth, typeof utl , typeof heads);
 
   if (!meth || !url || !heads) {
     return res.status(400).json({ error: "Champs requis manquants" });
